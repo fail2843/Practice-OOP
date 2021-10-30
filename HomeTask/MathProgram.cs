6ceba2f4b201bc1cda19cbcfe32aa4a0588999f9
fail2843 <83495514+fail2843@users.noreply.github.com>
2021-10-30 13:40:32 +0700

    Lesson-1

---
+++ HomeTask/MathProgram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask
+{
+    class MathProgram
+    {
+        private string _input;
+        private int _number;
+        private int _factorial;
+        private int _sum;
+        private int _maxEven;
+        public void Begin()
+        {
+            Console.WriteLine(" Здравствуйте, вас приветствует математическая программа!\n" +
+                              " -> Для старта нажмите 'b'\n" +
+                              " -> Для выхода из программы нажмите любую клавишу");
+            _input = Console.ReadLine();
+            switch (_input)
+            {           
+                case "b":
+                    Console.Clear();
+                    do
+                    {
+                        Console.Write(" Введите число: ");
+                        _input = Console.ReadLine();
+                    }
+                    while (!Int32.TryParse(_input, out _number));
+                    GetProgramResult();
+                    ShowAnswer();
+                    return;
+                default:
+                    Console.WriteLine(" Завершение программы!");
+                    Console.ReadKey();
+                    return;
+            }
+        }
+        private void GetProgramResult()
+        {
+            _sum = 0;
+            _factorial = 1;
+
+            for(int i = 1; i <= _number; i++)
+            {
+                _sum += i;
+                _factorial *= i;
+            }
+            if (_number % 2 == 0) _maxEven = _number;
+            else _maxEven = _number - 1;
+        }
+        private void ShowAnswer()
+        {
+            Console.WriteLine($" Факториал равен {_factorial}\n" +
+                              $" Сумма чисел от 1 до введенного равна {_sum}\n" +
+                              $" Максимальное четное число меньше введенного равно {_maxEven}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MathProgram().Begin();
+        }
+    }
+}
